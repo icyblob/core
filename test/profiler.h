1b1d571f89bb58c7d59d5e095ee57914d7a16c1c
cyber-pc <165458555+cyber-pc@users.noreply.github.com>
2024-07-16 20:23:28 +0700

    Add profiler

---
+++ test/profiler.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "string"
+
+#define ENABLE_PROFILER 0
+
+class Profiler {
+public:
+    Profiler(std::string name);
+
+    ~Profiler();
+
+    static void PrintReport(bool ms = true);
+
+private:
+    class Impl;
+
+    Impl* mImpl;
+};
+
+#if ENABLE_PROFILER
+#define PROFILE_SECTION(name, ...) std::unique_ptr<Profiler> p_profiler(new Profiler(name));
+#define PROFILE_SECTION_END() p_profiler.reset();
+#define PROFILE_PRINT_REPORT() Profiler::PrintReport()
+#else
+#define PROFILE_SECTION(name, ...)
+#define PROFILE_SECTION_END()
+#define PROFILE_PRINT_REPORT
+#endif
